3ac9baf460ebfa52f230e8460d3ca110f9934cb1
박준영 <wnsdud6969@naver.com>
2023-06-04 14:21:38 +0900

    test: 데코레이터 패턴 테스트

--- src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
+++ src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
@@ -2,6 +2,7 @@ package hello.proxy.pureproxy.decorator;
 
 import hello.proxy.pureproxy.decorator.code.Component;
 import hello.proxy.pureproxy.decorator.code.DecoratorPatternClient;
+import hello.proxy.pureproxy.decorator.code.MessageDecorator;
 import hello.proxy.pureproxy.decorator.code.RealComponent;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
@@ -15,4 +16,11 @@ public class DecoratorPatternTest {
         client.execute();
     }
 
+    @Test
+    void decorator1() {
+        Component realComponent = new RealComponent();
+        Component messageDecorator = new MessageDecorator(realComponent);
+        DecoratorPatternClient client = new DecoratorPatternClient(messageDecorator);
+        client.execute();
+    }
 }
