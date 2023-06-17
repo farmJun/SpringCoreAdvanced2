591703c6f6f12c484a620d87374936cada91a8ed
박준영 <wnsdud6969@naver.com>
2023-06-17 16:21:38 +0900

    test: CGLIB 예제 1 구현

--- src/test/java/hello/proxy/jdkdynamic/common/service/ConcreteService.java
+++ src/test/java/hello/proxy/jdkdynamic/common/service/ConcreteService.java
@@ -3,7 +3,7 @@ package hello.proxy.jdkdynamic.common.service;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class concreteService {
+public class ConcreteService {
     public void call() {
         log.info("ConcreteService 호출");
     }
